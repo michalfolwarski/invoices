8f828c1d3e45083028f3e603cbfd706af219571e
Michał Folwarski <folwarski.michal@gmial.com>
2018-05-04 15:03:21 +0200

    adding address class

---
+++ src/main/java/com/example/dao/entity/Address.java
@@ -0,0 +1,50 @@
+package com.example.dao.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+    private String street;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+    private String city;
+
+    public Address() {
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "street='" + street + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
