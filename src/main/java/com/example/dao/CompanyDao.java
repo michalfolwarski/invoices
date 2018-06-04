3982e2bf33123571f338042c35df0c129c9337c6
Michał Folwarski <folwarski.michal@gmial.com>
2018-06-04 19:30:00 +0200

    adding transaction

--- src/main/java/com/example/dao/CompanyDao.java
+++ src/main/java/com/example/dao/CompanyDao.java
@@ -2,6 +2,7 @@ package com.example.dao;
 
 import com.example.dao.entity.Company;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -14,17 +15,24 @@ public class CompanyDao implements Dao<Company>{
 
     @Override
     public int add(Company company){
-        return (int) session.save(company);
+        Transaction transaction = session.beginTransaction();
+        int id = (int) session.save(company);
+        transaction.commit();
+        return id;
     }
 
     @Override
     public void delete(Company company) {
+        Transaction transaction = session.beginTransaction();
         session.delete(company);
+        transaction.commit();
     }
 
     @Override
     public void update(Company company) {
+        Transaction transaction = session.beginTransaction();
         session.update(company);
+        transaction.commit();
     }
 
     @Override
